6a8e504eac8af5fbebe5db22cd173000d83394da
charliekaks <kaksunchained@gmail.com>
2018-07-02 16:49:15 +0300

    installed   a body parser to interpret http requests Added a post request to handle the posted messages

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-$(()=>{
+
+$(function() {
     $("#send").click(()=>{
-        addMessages({name:"charles", messageDetails:"hello"})
+        var message = {name: $("#name").val(), message: $("#message").val()};
+        postMessage(message); 
     })
-})
+    getMessages();
+});
+
 function addMessages(message) {
-    $("#messages").append(`<h3>${message.name}</h3> <p>${message.messageDetails}</p>`)
+    $("#messages").append(`<h3>${message.name}</h3> <p>${message.message}</p>`)
 }
 function getMessages(data){
-    $.get()
+    $.get("http://localhost:3000/messages", (data) => {
+            console.log(data);
+            data.forEach(element => {
+               addMessages(element) 
+            });
+    })
+}
+
+function postMessage(message){
+    $.post("http://localhost:3000/messages", message)
 }
--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 var express = require("express")
-
+var bodyParser = require("body-parser")
 var app = express()
 
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
 app.use(express.static(__dirname))
-var messages = [{name: " chuck", messageDetails: "How are you"}, {name: "Lousie", messageDetails: "Fine"}]
+var messages = [{name: " chuck", message: "How are you"}, {name: "Lousie", message: "Fine"}]
 
 app.get("/messages", (req, res) =>{
-    res.send("hello");
+    res.send(messages);
+})
+
+app.post("/messages", (req, res) =>{
+    console.log(req.body);
+    res.sendStatus(200)
+    messages.push(req.body)
 })
 
 var server = app.listen(3000, ()=>{
